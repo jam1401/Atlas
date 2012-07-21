333e3543fdc4988326be85e634271df9d60e6e66
Jeff Morgan <jam1401@gmail.com>
2012-07-21 08:03:00 -0700

    Simple Application is now working....

--- www/javascripts/App.js
+++ www/javascripts/App.js
@@ -1,85 +1,21 @@
-
-/*window.HomeView = Backbone.View.extend({
-
-  template:_.template($('#home').html()),
-
-  render:function (eventName) {
-    $(this.el).html(this.template());
-    return this;
+function onBodyLoad() {
+  if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
+    document.addEventListener("deviceready", onDeviceReady, false);
+  } else {
+    onDeviceReady();
   }
-});
+ // document.addEventListener("deviceready", onDeviceReady, false);
+}
 
-window.Page1View = Backbone.View.extend({
+/* When this function is called, PhoneGap has been initialized and is ready to roll */
+/* If you are supporting your own protocol, the var invokeString will contain any arguments to the app launch.
+ see http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+ for more details -jm */
+function onDeviceReady() {
+  // do your thing!
+  //navigator.notification.alert("PhoneGap is working")
 
-  template:_.template($('#page1').html()),
-
-  render:function (eventName) {
-    $(this.el).html(this.template());
-    return this;
-  }
-});
-
-window.Page2View = Backbone.View.extend({
-
-  template:_.template($('#page2').html()),
-
-  render:function (eventName) {
-    $(this.el).html(this.template());
-    return this;
-  }
-});
-
-var AppRouter = Backbone.Router.extend({
-
-  routes:{
-    "":"home",
-    "page1":"page1",
-    "page2":"page2"
-  },
-
-  initialize:function () {
-    // Handle back button throughout the application
-    $('.back').live('click', function(event) {
-      window.history.back();
-      return false;
-    });
-    this.firstPage = true;
-  },
-
-  home:function () {
-    console.log('#home');
-    this.changePage(new HomeView());
-  },
-
-  page1:function () {
-    console.log('#page1');
-    this.changePage(new Page1View());
-  },
-
-  page2:function () {
-    console.log('#page2');
-    this.changePage(new Page2View());
-  },
-
-  changePage:function (page) {
-    $(page.el).attr('data-role', 'page');
-    page.render();
-    $('body').append($(page.el));
-    var transition = $.mobile.defaultPageTransition;
-    // We don't want to slide the first page
-    if (this.firstPage) {
-      transition = 'none';
-      this.firstPage = false;
-    }
-    $.mobile.changePage($(page.el), {changeHash:false, transition: transition});
-  }
-
-});
-
-$(document).ready(function () {
   console.log('document ready');
   app = new AppRouter();
   Backbone.history.start();
-});
-
-*/
+}
